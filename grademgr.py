71ccb152d0478099eafc3669ddb2481012f0be44
Jose de Sousa <jose.t.de.sousa@gmail.com>
2016-12-11 22:54:52 +0000

    improve algorithm

--- grademgr.py
+++ grademgr.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python
 import csv, sys
 
 #define eval items here
-items = ["Lab1","Lab2","Lab3","Lab4","Lab5","Lab6"]
+#items = ["Lab1","Lab2","Lab3","Lab4","Lab5","Lab6"]
+items = []
 
 #define group number of elements range(min,max+1) here
 groupr = range(1,4)
@@ -12,33 +14,48 @@ grader = range(0,21)
 
 #global variables 
 data = []
-groups = []
+agrupamentos = []
 
 ####################################################################
-def listGroups():
-    for group in groups:
-        sys.stdout.write(group+' ')
-    print '\n'
+def saveDB(filename):
+            keys = ['N\xfamero', 'Nome']+agrupamentos+items
+            with open(filename, 'wb') as f:
+                writer = csv.DictWriter(f, keys, extrasaction='ignore')
+                writer.writeheader()
+                writer.writerows(data)
+                f.close()
+####################################################################
+def loadDB(filename):
+    with open(filename,  'r') as f:
+        reader = csv.DictReader(f, delimiter=';')
+        for line in reader:
+            data.append(line)
+        f.close()
+    agrupamentos = [s for s in data[0].keys() if "Agrup" in s]
 
 ####################################################################
-def listStudent(student):
-    try:
-        b = student.pop(0)
-    except:
-        return
+def listStudent(studentNumber):
+    return [item for item in data if item["N\xfamero"] == studentNumber]
+####################################################################
+def insertItem(itemname):
+    items.append(itemname)
 
-    studentFound = 0;
-    for student in data:
-        if student['Number'] == b:
-            print b
-            for y in student:
-                print y+' : '+student[y]        
-            studentFound = 1
-            return
+####################################################################
+def listGroupElements(agrupamento, groupname):
+    return [item for item in data if item[agrupamento] == groupname]
+####################################################################
+def getGroupInfo(studentNumber):
+    student = listStudent(studentNumber).pop(0)
+    for agrupamento in agrupamentos:
+        if student[agrupamento] != '':
+            break
+    groupInfo = []
+    groupInfo.append(agrupamento)
+    groupInfo.append(student[agrupamento])
+    return groupInfo
 
-    if studentFound == 0:
-        print "\nERROR: Student not found\n"
-        return 0
+
+
 
 ####################################################################
 def insertStudentGrade (studentGrade):
@@ -58,7 +75,7 @@ def insertStudentGrade (studentGrade):
 
     studentFound = 0;
     for student in data:
-        if student['Number'] == b[0]:
+        if student['N\xfamero'] == b[0]:
             student[b[1]] = b[2]
             studentFound = 1
             return 1
@@ -99,48 +116,16 @@ def insertGroupGrade (groupGrade) :
 
     return 1
 
-####################################################################
-def insertGroup (group) :
-    b = group
-
-    if not (len(b) in range(3,max(groupr)+2)):
-        print "\nERROR: Number of fields must be in range 2 to "+str(max(groupr)+2)+"\n"
-        return 0
-        
-    if b[0] in groups:
-        print "\nERROR: Group already exists\n"
-        return 0
-
-    groupName = b[0]
-    b.pop(0)
-
-    #check all members
-    for member in b:
-        studentFound = 0
-        for student in data:
-            if student['Number'] == member:
-                if(student['Group'] == 'NA'):
-                    studentFound = 1
-                    continue
-                else:
-                    print "\nERROR: Group member "+member+" already belongs in a group"
-                    return 0
-
-        if studentFound == 0:
-            print "Group member "+member+" not found\n"
-            return 0
-
-    #create group and insert all members
-    groups.append(groupName)
-    for member in b:
-        for student in data:
-            if student['Number'] == member:
-                student['Group'] = groupName
-                continue
-
-    return 1
 
 def main () :
+
+    if(len(sys.argv) != 2):
+        print 'usage: gradmgr infile'
+        sys.exit(1)
+
+    loadDB(sys.argv[1])
+
+    filename = sys.argv[1].split('.').pop(0)
 
     while 1:
         try:
@@ -151,60 +136,34 @@ def main () :
         cmdline = cmdline.split()
         cmd = 'NA'
         try:
-            cmd = cmdline.pop(0);
+            cmd = cmdline.pop(0)
         except:
             pass
 
-        if cmd=='isg':
+        if cmd=='ls':
+            student = listStudent(cmdline[0])
+            if student:
+                print student.pop(0)
+            
+        elif cmd=='isg':
             flag = insertStudentGrade(cmdline)
             if flag == 1:
                 print "Student grade inserted successfully\n"
             else:
                 print "Could not insert student grade\n"
-        elif cmd=='ls':
-            listStudent(cmdline)
-        elif cmd=='lg':
-            listGroups()
+        elif cmd=='ii':
+            insertItem(cmdline[0])
         elif cmd=='igg':
             flag = insertGroupGrade(cmdline)
             if flag == 1:
                 print "Group grade inserted successfully\n"
             else:
                 print "Could not insert group grade\n"
-        elif cmd=='ig':
-            flag = insertGroup(cmdline)
-            if flag == 1:
-                print "Group inserted successfully\n"
-            else:
-                print "Could not insert group\n"
-        elif cmd=='save':
-            keys = ['Number', 'Name', 'Group']
-            for item in items:
-                keys.append(item)
-            with open(cmdline[0], 'wb') as f:
-                writer = csv.DictWriter(f, keys, extrasaction='ignore')
-                writer.writeheader()
-                writer.writerows(data)
-                f.close()
-        elif cmd=='load':
-            with open(cmdline[0],  'r') as f:
-                reader = csv.DictReader(f, delimiter=';')
-                for line in reader:
-                    line['Number'] = line.pop('N\xfamero')
-                    line['Name'] = line.pop('Nome')
-                    for item in items:
-                        if not (item in line):
-                            line[item] = 'NA'
-                    try:
-                        line['Group'] = line['Group']
-                    except:
-                        line['Group'] = 'NA'
-
-                    data.append(line)
-                f.close()
         elif cmd=='quit':
+            saveDB(filename+'_out.csv')
             sys.exit(0)
         else:
             print 'Command not found'
                 
 if __name__ == "__main__" : main ()
+
