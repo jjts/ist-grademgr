ad2da3d17410fb2f291da716082a73c20beedde5
Jose T. de Sousa <jts@inesc-id.pt>
2016-12-12 18:24:26 +0000

    improve

--- grademgr.py
+++ grademgr.py
@@ -24,6 +24,7 @@ def saveDB(filename):
                 writer.writeheader()
                 writer.writerows(data)
                 f.close()
+
 ####################################################################
 def loadDB(filename):
     with open(filename,  'r') as f:
@@ -34,15 +35,9 @@ def loadDB(filename):
     agrupamentos = [s for s in data[0].keys() if "Agrup" in s]
 
 ####################################################################
-def listStudent(studentNumber):
-    return [item for item in data if item["N\xfamero"] == studentNumber]
-####################################################################
-def insertItem(itemname):
-    items.append(itemname)
-
-####################################################################
 def listGroupElements(agrupamento, groupname):
     return [item for item in data if item[agrupamento] == groupname]
+
 ####################################################################
 def getGroupInfo(studentNumber):
     student = listStudent(studentNumber).pop(0)
@@ -55,66 +50,83 @@ def getGroupInfo(studentNumber):
     return groupInfo
 
 
-
+    
+####################################################################
+# ls student_number
+####################################################################
+def listStudent(studentNumber):
+    return [item for item in data if item["N\xfamero"] == studentNumber]
 
 ####################################################################
-def insertStudentGrade (studentGrade):
-    b = studentGrade
+# ii item
+####################################################################
+def insertItem(itemname):
+    items.append(itemname)
 
-    if len(b) != 3:
+####################################################################
+# isg item student_number grade
+####################################################################
+def insertStudentGrade (studentGrade):
+
+    if len(studentGrade) != 3:
         print "\nERROR: Number of fields must be 3\n"
         return 0
         
-    if not (b[1] in items):
+    item = studentGrade[0]
+    if not (item in items):
         print "\nERROR: Item does not exist\n"
         return 0
 
-    if not (int(b[2]) in grader):
+    grade = studentGrade[2]
+    if not (int(grade) in grader):
         print "\nERROR: Grade is not in the range "+str(min(grader))+" "+str(max(grader))+"\n"
         return 0
 
-    studentFound = 0;
-    for student in data:
-        if student['N\xfamero'] == b[0]:
-            student[b[1]] = b[2]
-            studentFound = 1
-            return 1
+    student_number = studentGrade[1]    
+    student_list = [ student for student in data if student['N\xfamero'] == student_number ]
 
-    if studentFound == 0:
+    try:
+        student = student_list.pop(0)
+    except:
         print "\nERROR: Student not found\n"
-        return 0
+        return 1
+
+    student[item] = grade
+    return 0
 
 ####################################################################
-def insertGroupGrade (groupGrade) :
-    b = groupGrade
+# igg item group_member grade
+####################################################################
+def insertGroupGrade (groupGrade):
+            b = groupGrade
 
-    if len(b) != 3:
-        print "\nERROR: Number of fields is not 3\n"
-        return 0
+            if len(b) != 3:
+                        print "\nERROR: Number of fields is not 3\n"
+                        return 0
         
-    if not (b[0] in groups):
-        print "\nERROR: Group does not exist\n"
-        return 0
+            if not (b[0] in groups):
+                        print "\nERROR: Group does not exist\n"
+                        return 0
 
-    if not (b[1] in items):
-        print "\nERROR: Item does not exist\n"
-        return 0
+            if not (b[1] in items):
+                        print "\nERROR: Item does not exist\n"
+                        return 0
 
-    if not (int(b[2]) in grader):
-        print "\nERROR: Grade is not in the range "+str(min(grader))+" "+str(max(grader))+"\n"
-        return 0
+            if not (int(b[2]) in grader):
+                        print "\nERROR: Grade is not in the range "+str(min(grader))+" "+str(max(grader))+"\n"
+                        return 0
 
-    studentFound = 0;
-    for student in data:
-        if student['Group'] == b[0]:
-            student[b[1]] = b[2]
-            studentFound = 1
+            studentFound = 0;
+            for student in data:
+                        if student['Group'] == b[0]:
+                                    student[b[1]] = b[2]
+                                    studentFound = 1
 
-    if studentFound == 0:
-        print "\nERROR: Group empty or non-existing\n"
-        return 0
+            if studentFound == 0:
+                        print "\nERROR: Group empty or non-existing\n"
+                        return 0
 
-    return 1
+            return 1
 
 
 def main () :
